886562b33d358c9b6d93ede98dcd4dc58cd1af4e
Cristian <cristiano.rosetti@gmail.com>
2016-02-18 08:38:29 -0600

    =Articles and post view made

--- client/app/components/post/post-detail.compnt.ts
+++ client/app/components/post/post-detail.compnt.ts
@@ -1,41 +1,22 @@
 import {Component,ElementRef,
         Inject,Input}         from 'angular2/core';
-import {RouteParams, Router} from 'angular2/router';
-
+import {RouteParams, Router}  from 'angular2/router';
 import {Observable}           from 'rxjs/Observable';
 import {Observer}             from 'rxjs/Observer';
-
 import {AppState}             from '../../logic/AppState';
 import {dispatcher,state}     from '../../logic/StateAndDispatcher';
 import {Action,AddPostAction,
         DeletePostAction}     from '../../logic/Actions';
-
 import {PostService}          from '../../services/PostService.service';
 import {Logger}               from '../../services/Logger.service';
 import {Post}                 from '../../models/Post';
 
+declare var jQuery:any;
+declare var foundation:any;
+
 @Component({
-  template: `
-  <div *ngIf="post">
-    <div class="row medium-8 large-7 columns">
-      <div class="blog-post">
-        <h3>{{post.title}} <small>3/6/2015</small></h3>
-        <img class="thumbnail" src="{{post.img}}">
-        <p>{{post.content}}</p>
-        <div class="callout">
-          <ul class="menu simple">
-            <li><a href="#">Author: Mike Mikers</a></li>
-            <li><a href="#">Comments: 3</a></li>
-          </ul>
-        </div>
-      </div>
-    </div>
-  </div>
-  <div *ngIf="!post">
-    <h3>Blog Post Not Found </h3>
-  </div>
-  <button (click)="gotoPosts()">Back</button>
-  `,
+  selector    : 'post-detail',
+  templateUrl : 'app/components/post/templates/post-detail.html'
 })
 export class PostDetailCompnt {
 
@@ -52,6 +33,7 @@ export class PostDetailCompnt {
             ) {}
 
   ngOnInit() {
+    jQuery(this._elementRef.nativeElement).foundation();
     let id = this._routeParams.get('id');
     this._postService.get(id)
         .subscribe(
